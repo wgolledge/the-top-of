62765c2ca86c3ef93bd79abfb10e11639ae60e25
wgolledge <wiggiumg@gmail.com>
2019-03-30 12:20:57 +0000

    updated eslint, added materialUi, changed to sass & css-in-js

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
   env: {
     browser: true,
-    jest: true
+    jest: true,
   },
-  extends: ['airbnb', 'plugin:react/recommended'],
+  extends: ['airbnb', 'plugin:react/recommended', 'prettier', 'prettier/react'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['import', 'react', 'import', 'jsx-a11y', 'prettier'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
@@ -24,8 +24,9 @@ module.exports = {
     'prettier/prettier': [
       'error',
       {
-        singleQuote: true
-      }
-    ]
-  }
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+  },
 };
