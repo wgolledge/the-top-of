3cd78b714c145e8d7e73af3bf8022c2995d9c42d
wgolledge <wiggiumg@gmail.com>
2019-03-30 12:20:57 +0000

    updated eslint, added materialUi, changed to sass & css-in-js

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { get } from 'axios';
-import './App.css';
+import { makeStyles } from '@material-ui/styles';
+
+import Header from './components/Header';
+import Button from './components/Button';
+
+const useStyles = makeStyles({
+  root: {
+    height: '100%',
+  },
+  main: {
+    alignItems: 'center',
+    display: 'flex',
+    height: props => `calc(100% - ${props.height}px)`,
+    justifyContent: 'center',
+  },
+});
 
 const App = () => {
+  const [height, setHeight] = useState(null);
   const [text, setText] = useState('...');
+
+  const classes = useStyles({ height });
 
   useEffect(() => {
     get('http://localhost:8080/').then(({ data }) => setText(data));
   }, []);
 
-  return <div className="App">{`show me ${text}`}</div>;
+  return (
+    <div className={classes.root}>
+      <Header setHeight={setHeight} />
+      {height && (
+        <div className={classes.main}>
+          <Button text={text} />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
