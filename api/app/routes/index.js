196c6566a810457861eaaf6a0d88ea1bee66d56d
wgolledge <wiggiumg@gmail.com>
2019-03-31 22:06:15 +0100

    added stubbed data to api

--- api/app/routes/index.js
+++ api/app/routes/index.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line no-multi-assign
-const app = module.exports = require('express')();
+const app = (module.exports = require('express')());
 
 app.get('/', (req, res) => {
   res.send('... the top of');
 });
+
+app.use('/sources', require('./sources'));
