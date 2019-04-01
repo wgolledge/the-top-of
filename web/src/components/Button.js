46dcc875c6feaaac3d9cbc6bf0d8d79e4bb199c4
Will Golledge <wiggiumg@gmail.com>
2019-04-01 17:07:22 +0100

    added basic modal and list of sources, changed eslint parser settings

--- web/src/components/Button.js
+++ web/src/components/Button.js
@@ -2,14 +2,19 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 
-const button = ({ text }) => (
-  <Button variant="contained" color="primary">
+const button = ({ text, onClick }) => (
+  <Button onClick={onClick} variant="contained" color="primary" size="large">
     {text}
   </Button>
 );
 
 button.propTypes = {
   text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+button.defaultProps = {
+  onClick: undefined,
 };
 
 export default button;
