e7aacc4a9b0e546b068f3af9f60890769b4571c2
Will Golledge <wiggiumg@gmail.com>
2019-04-02 16:22:21 +0100

    added setup with live data, changed eslint settings for api

---
+++ api/app/actions/sources.js
@@ -0,0 +1,36 @@
+const supportedSources = require('./supportedSources/supportedSources');
+
+const returnPropIfExists = (prop, name) => prop && { [name]: prop };
+
+const getSources = (req, res) =>
+  res.send(
+    supportedSources.reduce((acc, curr) => {
+      if (!curr.working) {
+        return acc;
+      }
+
+      acc.push({ id: curr.id, name: curr.name });
+
+      return acc;
+    }, []),
+  );
+
+const getSourceData = (req, res) => {
+  const { getData, attributionLink } = supportedSources.find(
+    source => source.id === Number(req.params.id),
+  );
+
+  return getData()
+    .then(data =>
+      res.send({
+        data,
+        ...returnPropIfExists(attributionLink, 'attributionLink'),
+      }),
+    )
+    .catch(err => console.log(err));
+};
+
+module.exports = {
+  getSources,
+  getSourceData,
+};
