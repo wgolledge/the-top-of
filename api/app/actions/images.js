9ac95ffb8b5992d0140254b57a7addb25e5cb80f
Will Golledge <wiggiumg@gmail.com>
2019-04-03 12:04:53 +0100

    added images to api, frontend now references image dynamically, fixed change source button

---
+++ api/app/actions/images.js
@@ -0,0 +1,12 @@
+const supportedSources = require('./supportedSources/supportedSources');
+
+const getSourceImage = (req, res) => {
+  const imageId = Number(req.params.id);
+  const { imgName } = supportedSources.find(source => source.id === imageId);
+
+  res.sendFile(`${__dirname}/supportedSources/images/${imgName}`);
+};
+
+module.exports = {
+  getSourceImage,
+};
