374df5c7a5e495f8c34f18fe1e3ae609fc8cbc41
Will Golledge <35961363+wgolledge@users.noreply.github.com>
2019-04-07 11:05:35 +0100

    Feature/change fe design (#7)
    * added body component and moved SourceCard out of Modal
    
    * responsiveness changes

--- web/src/components/SourceList.js
+++ web/src/components/SourceList.js
@@ -1,43 +1,59 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles, useTheme } from '@material-ui/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import { createMuiTheme } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
-const theme = createMuiTheme();
-
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
-    width: '100%',
-    maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
+    height: '100%',
+    overflow: 'scroll',
+  },
+  list: {
+    '&&': {
+      [theme.breakpoints.down('sm')]: {
+        paddingTop: 0,
+      },
+    },
   },
   listItemText: {
-    fontSize: '0.5em',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: 'calc(0.7vw + 0.7vh + .5vmin)',
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: 'calc(0.6vw + 0.6vh + .5vmin)',
+    },
+    [theme.breakpoints.up('lg')]: {
+      fontSize: '1rem',
+    },
   },
-});
+}));
 
 function ListItemLink(props) {
   return <ListItem button component="a" {...props} />;
 }
 
 function SourceList({ articles }) {
-  const classes = useStyles();
-
-  console.log(classes);
+  const classes = useStyles(useTheme());
 
   return (
     <div className={classes.root}>
-      <List component="nav">
+      <List component="nav" className={classes.list}>
         {articles.map(article => (
-          <ListItemLink href={article.url} key={article.id}>
-            <ListItemText
-              primary={<Typography variant="body1">{article.title}</Typography>}
-            />
-          </ListItemLink>
+          <div key={article.id}>
+            <ListItemLink href={article.url}>
+              <ListItemText
+                primary={article.title}
+                className={classes.listItemText}
+                disableTypography
+              />
+            </ListItemLink>
+            <Divider />
+          </div>
         ))}
       </List>
     </div>
