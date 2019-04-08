12bd7b1d9b9c7b1332568be390325a0753994547
Will Golledge <wiggiumg@gmail.com>
2019-04-08 15:26:22 +0100

    updated to use node-persist and added reddit

--- api/app/actions/supportedSourceList.js
+++ api/app/actions/supportedSourceList.js
@@ -1,19 +1,54 @@
 const glob = require('glob');
 const path = require('path');
+const storage = require('node-persist');
+const schedule = require('node-schedule');
+
+storage.init();
 
 const sources = glob.sync(`${__dirname}/supportedSources/*.js`).map(file =>
   // eslint-disable-next-line
   require(path.resolve(file))
 );
 
-let id = 0;
+const availableSources = (() => {
+  let id = 0;
 
-module.exports = sources.map(source => ({
-  // eslint-disable-next-line no-plusplus
-  id: ++id,
-  name: source.name,
-  working: source.working,
-  imgName: source.imgName,
-  getData: source.getData,
-  attributionLink: source.attributionLink,
-}));
+  return sources
+    .filter(source => source.working)
+    .map(source => ({
+      // eslint-disable-next-line no-plusplus
+      id: ++id,
+      name: source.name,
+      working: source.working,
+      imgName: source.imgName,
+      getData: source.getData,
+      attributionLink: source.attributionLink,
+    }));
+})();
+
+const updateSourceStorage = async () => {
+  console.log('updating sourceStorage');
+
+  try {
+    const sourceDataArr = await Promise.all(
+      availableSources.map(source => source.getData()),
+    );
+
+    await availableSources.forEach((source, i) => {
+      storage.setItem(source.id.toString(), sourceDataArr[i]);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+updateSourceStorage();
+
+// update sourceData every two hours
+schedule.scheduleJob('0 */2 * * *', () => {
+  updateSourceStorage();
+});
+
+module.exports = {
+  availableSources,
+};
