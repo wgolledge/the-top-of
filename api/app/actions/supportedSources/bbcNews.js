36c78dd6eaee6a573ffc5ac3bb91f6a9d6ed0311
wgolledge <wiggiumg@gmail.com>
2019-04-16 21:10:13 +0100

    added dailymail and bbc sources

---
+++ api/app/actions/supportedSources/bbcNews.js
@@ -0,0 +1,30 @@
+const Parser = require('rss-parser');
+
+const parser = new Parser();
+
+module.exports = {
+  name: 'BBC News',
+  working: true,
+  imgName: 'bbcNews.png',
+  url: 'https://bbc.co.uk',
+  priority: 30,
+  getData: () =>
+    parser
+      .parseURL('http://feeds.bbci.co.uk/news/rss.xml')
+      .then(({ items }) => {
+        let id = 0;
+
+        return items
+          .reduce((acc, { title, link: url }) => {
+            if (!url) {
+              return acc;
+            }
+
+            // eslint-disable-next-line no-plusplus
+            acc.push({ id: ++id, title, url });
+            return acc;
+          }, [])
+          .slice(0, 10);
+      })
+      .catch(err => console.log(err)),
+};
---
+++ api/app/actions/supportedSources/dailymail.js
@@ -0,0 +1,36 @@
+const Parser = require('rss-parser');
+
+const parser = new Parser({
+  customFields: {
+    item: [['media:thumbnail', 'media:thumbnail', { keepArray: false }]],
+  },
+});
+
+module.exports = {
+  name: 'Daily Mail',
+  working: true,
+  imgName: 'dailymail.png',
+  url: 'https://dailymail.co.uk/',
+  priority: 40,
+  getData: () =>
+    parser
+      .parseURL('https://www.dailymail.co.uk/news/index.rss')
+      .then(({ items }) => {
+        let id = 0;
+
+        return items
+          .reduce((acc, { title, link: url, ...rest }) => {
+            if (!url) {
+              return acc;
+            }
+
+            const thumbnail = rest['media:thumbnail'].$.url;
+
+            // eslint-disable-next-line no-plusplus
+            acc.push({ id: ++id, title, url, thumbnail });
+            return acc;
+          }, [])
+          .slice(0, 10);
+      })
+      .catch(err => console.log(err)),
+};
