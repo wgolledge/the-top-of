ac3a7105dcc07bc6c15b22b1e0e2ab2719664719
wgolledge <wiggiumg@gmail.com>
2019-05-19 20:42:28 +0100

    added youtube source without thumbnails

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Welcome! The Top Of is a news aggregator that lets you see the top (10) stories from your favourite sites without having to visit each one individually. No signups or logins, just trending content from the site(s) you choose. 🙂
 
-The content is best viewed on a mobile device! Implemented as a PWA using service workers and Google workbox.
+The content is best viewed on a *mobile* device! Implemented as a PWA using service workers and Google workbox.
+
+*Disclaimer*: By using this site you are in turn agreeing to be bound by each of the sources terms of service. The current list of sources is displayed on the homepage.
 
 ## Running locally
 
---
+++ api/app/actions/supportedSources/youtube.js
@@ -0,0 +1,54 @@
+const { get } = require('axios');
+
+module.exports = {
+  name: 'YouTube',
+  working: true,
+  imgName: 'youtube.png',
+  url: 'https://youtube.com/',
+  priority: 120,
+  getData: () =>
+    get(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&key=${
+        process.env.API_KEY_YOUTUBE
+      }`,
+    )
+      .then(({ data: { items } }) => {
+        let id = 0;
+
+        return items
+          .reduce(
+            (
+              acc,
+              {
+                id: videoId,
+                snippet: {
+                  title,
+                  thumbnails: {
+                    default: { url: thumbnail },
+                  },
+                },
+              },
+            ) => {
+              if (!videoId) {
+                return acc;
+              }
+
+              acc.push({
+                // eslint-disable-next-line no-plusplus
+                id: id++,
+                title,
+                url: `https://www.youtube.com/watch?v=${videoId}`,
+                // thumbnail,
+              });
+              return acc;
+            },
+            [],
+          )
+          .slice(0, 10);
+      })
+      .catch(err => console.log(err)),
+  attributionLink: {
+    text: "YouTube's TOS",
+    link: 'https://www.youtube.com/t/terms/',
+  },
+};
